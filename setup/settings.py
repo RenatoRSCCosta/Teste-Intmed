fcb5ed4e9c0e52e154e458d8e01a552d824cbe17
Renato Costa <renatosilvacosta1998@gmail.com>
2022-07-29 21:07:37 -0300

    alter settings

--- setup/settings.py
+++ setup/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-2y_#coy+1g)p0v01eeccswxq@3t0mbt9(^l+o2#rq)s0#z^l(' #os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG',False)
